363de721c8db4f6ed799a4a833b219490fdbff25
mr-sabya <sabya.citik@gmail.com>
2025-06-02 15:54:13 +0600

    why choose us section dynamic

--- app/Livewire/Frontend/Home/WhyChoose/Index.php
+++ app/Livewire/Frontend/Home/WhyChoose/Index.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire\Frontend\Home\WhyChoose;
 
+use App\Models\WhyChooseUs;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.frontend.home.why-choose.index');
+        return view('livewire.frontend.home.why-choose.index',[
+            'whyChooseUs' => WhyChooseUs::with('features')->firstOrFail(),
+        ]);
     }
 }
--- resources/views/livewire/frontend/home/why-choose/index.blade.php
+++ resources/views/livewire/frontend/home/why-choose/index.blade.php
@@ -3,41 +3,32 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="common-heading text-l">
-                    <span>Why Choose Us</span>
-                    <h2 class="mb20">Why The Niwax <span class="text-second text-bold">Ranked Top</span> Among The Leading Web & App Development Companies</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                    <span>{{ $whyChooseUs->section_heading }}</span>
+                    <h2 class="mb20">{{ $whyChooseUs->title_normal }} <span class="text-second text-bold">{{ $whyChooseUs->title_highlighted }}</span> {{ $whyChooseUs->title_suffix }}</h2>
+                    <p>{{ $whyChooseUs->description }}</p>
                     <div class="itm-media-object mt40 tilt-3d">
-                        <div class="media">
-                            <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"><img src="{{ url('assets/frontend/images/icons/computers.svg') }}" alt="icon" class="layer"></div>
+                        @foreach($whyChooseUs->features as $feature)
+                        <div class="media {{ $loop->first ? '' : 'mt40' }}">
+                            <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"><img src="{{ url('storage/' . $feature->icon) }}" alt="icon" class="layer"></div>
                             <div class="media-body">
-                                <h4>Streamlined Project Management</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
+                                <h4>{{ $feature->title }}</h4>
+                                <p>{{ $feature->description }}</p>
                             </div>
                         </div>
-                        <div class="media mt40">
-                            <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"><img src="{{ url('assets/frontend/images/icons/worker.svg') }}" alt="icon" class="layer"></div>
-                            <div class="media-body">
-                                <h4>A Dedicated Team of Experts</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
-                            </div>
-                        </div>
-                        <div class="media mt40">
-                            <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"> <img src="{{ url('assets/frontend/images/icons/deal.svg') }}" alt="icon" class="layer"></div>
-                            <div class="media-body">
-                                <h4>Completion of Project in Given Time</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
-                            </div>
-                        </div>
+                        @endforeach
+
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
-                <div data-tilt data-tilt-max="5" data-tilt-speed="1000" class="single-image bg-shape-dez wow fadeIn" data-wow-duration="2s"><img src="{{ url('assets/frontend/images/about/about-company.jpg') }}" alt="image" class="img-fluid"></div>
-                <p class="text-center mt30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                <div data-tilt data-tilt-max="5" data-tilt-speed="1000" class="single-image bg-shape-dez wow fadeIn" data-wow-duration="2s">
+                    <img src="{{ url('storage/' . $whyChooseUs->image) }}" alt="image" class="img-fluid">
+                </div>
+                <p class="text-center mt30">{{ $whyChooseUs->image_text }}</p>
                 <div class="cta-card mt60 text-center">
-                    <h3 class="mb20">Let's Start a <span class="text-second text-bold">New Project</span> Together</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper.</p>
-                    <a href="#" class="btn-outline lnk mt30">Request A Quote <i class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
+                    <h3 class="mb20">{{ $whyChooseUs->cta_title_prefix }} <span class="text-second text-bold">{{ $whyChooseUs->cta_title_highlight }}</span> {{ $whyChooseUs->cta_title_suffix }}</h3>
+                    <p>{{ $whyChooseUs->cta_description }}</p>
+                    <a href="#" class="btn-outline lnk mt30">{{ $whyChooseUs->cta_button_text }} <i class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
                 </div>
             </div>
         </div>
